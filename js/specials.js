93514701a2f318c2395358d918ed0947c0889ed8
rawat.abhishek <abhishek.rawat@vinsol.com>
2019-10-15 13:14:13 +0530

    second commit

---
+++ js/specials.js
@@ -0,0 +1,38 @@
+class DaySpecial {
+  constructor(form, submitClass, specialsPath, specialDiv) {
+    this.form = $(form);
+    this.selectEle = this.form.find('select');
+    this.submitClass = submitClass;
+    this.specialsPath = specialsPath;
+    this.specialDiv = $(specialDiv);
+  }
+
+  removeSubmit = () => {
+    this.form.find(this.submitClass).remove();
+  };
+
+  loadSpecials = () => {
+    $.getJSON(this.specialsPath, (data)=>{this.specials = data});
+  };
+
+  showSpecials = (e) => {
+    console.log(this.specialDiv);
+    var specialInfo = this.specials[$(e.currentTarget).val()];
+    var divContent = $('<h3 />').text(specialInfo['title'])
+      .append($('<img />').attr('src',specialInfo['image'],'alt',"today's special"))
+      .append($('<p />').text(specialInfo['text']));
+    this.specialDiv.css('color',specialInfo['color']).html(divContent);
+  };
+
+  eventHandler = () => {
+    this.selectEle.change(this.showSpecials);
+  };
+
+  init = () => {
+    this.removeSubmit();
+    this.loadSpecials();
+    this.eventHandler();
+  };
+}
+
+(new DaySpecial('#specials form', '.buttons','data/specials.json', '#specialDiv')).init();
